163f583516cdc3a7c0c9df7e6308e40aae1862b4
Velocifero1052 <commelevent1052@yandex.ru>
2023-07-24 17:29:22 +0500

    room list as separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent
+    RoomsComponent,
+    RoomsListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Room, RoomList} from "./rooms";
 
 @Component({
@@ -6,7 +6,7 @@ import {Room, RoomList} from "./rooms";
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
   hotelName: string = 'Hilton Hotel';
 
   hidden: boolean = false;
@@ -17,39 +17,43 @@ export class RoomsComponent {
     totalRooms: 20,
     availableRooms: 15,
     bookedRooms: 5
-  }
+  };
 
-  roomList: RoomList[] = [{
-    roomNumber: 1,
-    roomType: 'Deluxe Room',
-    amenities: 'Air conditioned, One seated, Fridge',
-    price: 200,
-    'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
-    checkinTime: new Date('11-Nov-2023'),
-    checkoutTime: new Date('20-Nov-2023')
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Private Suite',
-      amenities: 'Air conditioned, Two seated, Bar only',
-      price: 500,
-      'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
-      checkinTime: new Date('01-Oct-2023'),
-      checkoutTime: new Date('05-Oct-2023')
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite Lux',
-      amenities: 'Air conditioned, Two seated, Fridge, Bar & TV set',
-      price: 1000,
-      'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
-      checkinTime: new Date('10-May-2023'),
-      checkoutTime: new Date('17-May-2023')
-    }
-  ]
+  roomList: RoomList[] = [];
 
   toggle(): void {
     this.hidden = !this.hidden;
+  }
+
+  ngOnInit(): void {
+    this.roomList = [{
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air conditioned, One seated, Fridge',
+      price: 200,
+      'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date('20-Nov-2023')
+    },
+      {
+        roomNumber: 2,
+        roomType: 'Private Suite',
+        amenities: 'Air conditioned, Two seated, Bar only',
+        price: 500,
+        'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
+        checkinTime: new Date('01-Oct-2023'),
+        checkoutTime: new Date('05-Oct-2023')
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite Lux',
+        amenities: 'Air conditioned, Two seated, Fridge, Bar & TV set',
+        price: 1000,
+        'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
+        checkinTime: new Date('10-May-2023'),
+        checkoutTime: new Date('17-May-2023')
+      }
+    ];
   }
 
 }
