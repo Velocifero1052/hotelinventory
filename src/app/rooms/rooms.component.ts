f745bf480e66bed9bb34d2a27439b9df57b29e92
Velocifero1052 <commelevent1052@yandex.ru>
2023-07-24 15:43:16 +0500

    some changes

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {Room, RoomList} from "./rooms";
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent {
+  hotelName: string = 'Hilton Hotel';
+
+  hidden: boolean = false;
+
+  roomsCount: number = 20;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 15,
+    bookedRooms: 5
+  }
+
+  roomList: RoomList[] = [{
+    roomNumber: 1,
+    roomType: 'Deluxe Room',
+    amenities: 'Air conditioned, One seated, Fridge',
+    price: 200,
+    'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
+    checkinTime: new Date('11-Nov-2023'),
+    checkoutTime: new Date('20-Nov-2023')
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Private Suite',
+      amenities: 'Air conditioned, Two seated, Bar only',
+      price: 500,
+      'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
+      checkinTime: new Date('01-Oct-2023'),
+      checkoutTime: new Date('05-Oct-2023')
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite Lux',
+      amenities: 'Air conditioned, Two seated, Fridge, Bar & TV set',
+      price: 1000,
+      'photos': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JbhsI6d_DpTV6QnMGzvO_wHaE8%26pid%3DApi&f=1&ipt=ad5cd3822793f728d906ed0ae2bfb429232301e3379e8718c4fcf5520f42caee&ipo=images',
+      checkinTime: new Date('10-May-2023'),
+      checkoutTime: new Date('17-May-2023')
+    }
+  ]
+
+  toggle(): void {
+    this.hidden = !this.hidden;
+  }
+
+}
